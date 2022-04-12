2c8529f473c6f440d98df8fc33c04aa2062eee3e
stantier <102152502+stantier@users.noreply.github.com>
2022-04-12 14:49:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,95 @@
+// Get post data
+let getPosts= ()=>{
 
-function getData(){
+    fetch('http://localhost:3000/employees').then(response => response.json())
+    .then((data) => {
+        console.log(data)
+        
+    // using map function
+    data.map((records)=>{
+        //console.log(records.EmployeeID)
+        let row=`<tr>
+        <td>${records.id}</td>
+                  <td>${records.first_name}</td>
+                  <td>${records.last_name}</td>
+                  <td>${records.email}</td>
+                  <td><a href="" onclick="edit(${records.id})">Edit</a></td>
+                  <td><a href="" >Delete Record</a></td>
+                  </tr>`
+                  document.getElementById("showData").innerHTML+=row;
+      })
 
-    let formData={
-        fname:document.querySelector("#fname").value,
-        lname:document.querySelector("#lname").value,
-     email:document.querySelector("#email").value,
-    pwd:document.querySelector("#pwd").value
+    
+    }).catch((err) => {
+        console.log(err);
+    });
+    
+}
+getPosts();
+
+
+// edit record
+
+let edit=(id)=>{
+    let users1={
+        first_name:document.querySelector("#first_name").value,
+        last_name:document.querySelector("#last_name").value,
+        email:document.querySelector("#email").value
 
     }
+    console.log(users1)
+    //alert(id)
+    fetch('http://localhost:3000/employees/id',{
+            method:"PUT",
+            body:JSON.stringify(users1)
+        })
+            .then(res=>res.json())
+            .then(json=>console.log(json))
+
+}
+
+
+
+
+//insert New Record
+
  
+let addNew=()=>{
    
-    userRecord.push(users)
-    console.log(JSON.stringify(userRecord))
+    // let first_name=document.querySelector("#first_name").value;
+    // let last_name=document.querySelector("#last_name").value
+    // let email=document.querySelector("#email").value
+    // alert(email)
+    let users={
+        first_name:document.querySelector("#first_name").value,
+        last_name:document.querySelector("#last_name").value,
+        email:document.querySelector("#email").value
 
-localStorage.setItem('username',JSON.stringify(userRecord))
-localStorage.setItem('pwd',pwd)
-alert("Registered")
+    }
+    console.log(users)
+   
 
-//window.location.href="login.html"
-}
-
-
-
-// login 
-function login(){
-    let email=document.querySelector("#email").value
-    let pwd=document.querySelector("#pwd").value
-
-    let emailuser=localStorage.getItem('username')
-    let userpass=localStorage.getItem('pwd')
-   // alert(emailuser)
-
-   if(email==emailuser && pwd== userpass){
-
-    alert("logged in")
     
-    window.location.href="dashboard.html"
 
-   }else{
-    alert("Invalid Username or password")
-   }
+ // document.querySelector("#regform").addEventListener("submit",addNew)
+
+let getData= {
+    method: "POST",
+    headers: {
+        "Content-type": "application/json; charset=UTF-8", 
+      },
+      body: JSON.stringify(users)
+
+}
+   fetch("http://localhost:3000/employees",getData) .then(res=>res.json())
+   .then(json=>console.log(json))
+ 
+
 
 }
 
 
-function logout(){
-
-    localStorage.removeItem('username')
-    localStorage.removeItem('pwd')
-    window.location.href="login.html"
+// Delete record
+let deleteRecord=(id)=>{
 
 }
